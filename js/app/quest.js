d63c7a2a6d803a79f839613b9b1e7b9fa5fc24ad
Helias <stefanoborzi32@gmail.com>
2015-05-12 23:21:28 +0200

    Implemented Quest Search

--- js/app/quest.js
+++ js/app/quest.js
@@ -2,7 +2,36 @@
 
   var app = angular.module('keira2');
 
-  app.controller("QuestController", function() {
+  app.controller("QuestController", function($scope, $http) {
+    $scope.search = function(questId, questTitle) {
+      var request = app.api + "quest/template/";
+
+      if((typeof questId !== "undefined") && (questId != ""))
+        request += questId;
+      else if(typeof questTitle !== "undefined")
+      {
+        if(questTitle.length < 4)
+        {
+          alert("Please insert a string of at least 4 characters.");
+          return;
+        }
+        request += questTitle;
+      }
+      else
+      {
+        alert("Insert ID or Title of a quest");
+        return;
+      }
+
+      $http.get( request )
+        .success(function(data, status, header, config) {
+        $scope.quests = data;
+      })
+        .error(function(data, status, header, config) {
+        console.log("Error in QUEST $http.get request");
+      });
+
+    };
   });
 
 })()
