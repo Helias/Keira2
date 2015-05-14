40556067167326ea3776c1487f58eb281c875eed
Helias <stefanoborzi32@gmail.com>
2015-05-14 20:40:31 +0200

    Updated creature_template

--- js/app/creature.js
+++ js/app/creature.js
@@ -21,29 +21,9 @@
 
     $scope.search = function (CreatureEntry, CreatureName, CreatureSubname) {
 
-      var request = app.api + "creature/template/";
-
-      if ((typeof CreatureEntry !== "undefined") && (CreatureEntry !== "")) {
-
-        request += CreatureEntry;
-
-      } else if (typeof CreatureName !== "undefined" && typeof CreatureSubname !== "undefined" && CreatureSubname.length >= 4) {
-
-        request += CreatureName+"/"+CreatureSubname;
-
-      } else if (typeof CreatureName !== "undefined") {
-
-        request += CreatureName;
-
-      } else {
-
-        alert("Insert Entry or Name of a creature");
-        return;
-
-      }
-
-      $http.get(request)
-        .success(function (data, status, header, config) {
+      $http.get( app.api + "/search/creature/", {
+        params: { id: CreatureEntry, name: CreatureName, subname: CreatureSubname }
+      }).success(function (data, status, header, config) {
         $scope.creatures = data;
       })
         .error(function (data, status, header, config) {
