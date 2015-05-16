43bc60c5330cf7793c56e26158c6348fcf721558
ShinDarth <borzifrancesco@gmail.com>
2015-05-16 11:08:23 +0200

    Proper handling of tabs

--- js/app/creature.js
+++ js/app/creature.js
@@ -8,19 +8,55 @@
 
   app.controller("CreatureController", function ($scope, $http, $stateParams, $modal) {
 
+    /* All Creature tabs, disabled by default.
+    *  Only one tab can be active at a time */
+    $scope.creatureTabs = {
+      search                : false,
+      creatureTemplate      : false,
+      creatureLocation      : false,
+      modelInfo             : false,
+      equipTemplate         : false,
+      creatureLoot          : false,
+      pickpocketLoot        : false,
+      skinLoot              : false,
+      creatureTemplateAddon : false,
+      npcGossip             : false,
+      onKillReputation      : false,
+      involvedIn            : false,
+      localesNpcText        : false,
+      creatureMovement      : false,
+      creatureAddon         : false,
+      script                : false
+    };
+
+    /* Check if a creature is selected */
     if ($stateParams.id) {
+
+      /* Retrieve all creature datas */
       $http.get( app.api + "creature/template/" + $stateParams.id)
         .success(function (data, status, header, config) {
+
+        /* current_* mantains the database state
+        *  new_*     mantains the editor state  */
         $scope.current_creature_template = data[0];
         $scope.new_creature_template = angular.copy($scope.current_creature_template);
+
         console.log($scope.current_creature_template);
       })
         .error(function (data, status, header, config) {
         console.log("Error in CREATURE DATA $http.get request");
       });
+
+      /* We have a creature selected and default active tab is creatureTemplate */
       $scope.isCreatureSelected = true;
+      $scope.creatureTabs.creatureTemplate = true;
+    } else {
+      /* We have no creature selected and default active tab is search */
+      $scope.isCreatureSelected = false;
+      $scope.creatureTabs.search = true;
     }
 
+    /* [Function] Search */
     $scope.search = function (CreatureEntry, CreatureName, CreatureSubname) {
 
       $http.get( app.api + "/search/creature/", {
@@ -38,7 +74,7 @@
 
     };
 
-    /* Generate SQL Script for Creature */
+    /* [Function] Generate SQL Script for Creature */
     $scope.generateCreatureScript = function() {
 
       if (!$scope.isCreatureSelected) { return; }
@@ -47,6 +83,20 @@
 
       $scope.creatureScript = app.getUpdateQuery("creature_template", whereCondition, $scope.current_creature_template, $scope.new_creature_template);
 
+    };
+
+    /* [Function] disactive all tabs */
+    $scope.disactiveAllTabs = function() {
+      angular.forEach($scope.creatureTabs, function(value, key) {
+        value = false;
+      });
+    };
+
+    /* [Function] open SQL Script tab */
+    $scope.openScriptTab = function() {
+      $scope.disactiveAllTabs();
+      $scope.generateCreatureScript();
+      $scope.creatureTabs.script = true;
     };
 
     $scope.open = function (size) {
