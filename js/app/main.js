978abb2cb6b65ba3f62448080a2f932b0f7cb5d9
ShinDarth <borzifrancesco@gmail.com>
2015-05-17 23:25:00 +0200

    Reset loot_template system

--- js/app/main.js
+++ js/app/main.js
@@ -44,59 +44,6 @@
 
     };
 
-    /* Loot_template */
-
-    // Initialize some variables needed for *_item_loot
-    $rootScope.itemLoot = {};
-    $rootScope.row = "";
-
-    /* [Functions] loot_template
-     * input:
-     * - rowId => index of the array new_*_loot_template that contain the property of the loot_item
-     */
-    $rootScope.selectRow = function(rowIdChild, itemLoot, loot_template) {
-
-      if ($rootScope.row !== "") {
-        angular.element($rootScope.row).removeClass("trSelected");
-      }
-
-      $rootScope.row = rowIdChild.target.parentNode.parentNode;
-      angular.element($rootScope.row).addClass("trSelected");
-      var rowId = $rootScope.row.id, i = 0, new_loot_template = angular.copy(loot_template);
-
-      for (i in new_loot_template[rowId]) {
-        itemLoot[i] = new_loot_template[rowId][i];
-      }
-
-      itemLoot.id = rowId;
-    };
-
-    // Add an item to *_item_loot
-    $rootScope.addItem_loot = function(rowId, itemLoot, new_loot_template) {
-      var i = 0, lootLen = new_loot_template.length;
-      for (i = 0; i < lootLen; i++)
-      {
-        if (new_loot_template[i].Item === itemLoot.Item)
-        {
-          alert("The item " + itemLoot.Item + " already exists in this loot_template, you can't add it, you can only delete or edit it");
-          return;
-        }
-      }
-
-      itemLoot.Entry = new_loot_template[0].Entry;
-      new_loot_template[lootLen] = angular.copy(itemLoot);
-    };
-
-    // Edit an item loot property
-    $rootScope.editItem_loot = function(rowId, itemLoot, new_loot_template) {
-      new_loot_template[rowId] = angular.copy(itemLoot);
-    };
-
-    // Remove an item to *_item_loot
-    $rootScope.deleteItem_loot = function(rowId, new_loot_template) {
-      new_loot_template.splice(rowId, 1);
-    };
-
 
     /* Open modal to show Full SQL Script */
     $rootScope.openFullScriptModal = function(lootObject, tableName, primaryKey1) {
