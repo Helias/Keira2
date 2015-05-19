f1c5f05988b815336ca5220bd95b886972d37d16
ShinDarth <borzifrancesco@gmail.com>
2015-05-19 18:26:53 +0200

    [Creature] Do not allow too generic searches when there is only 1 search param.

--- js/app/creature.js
+++ js/app/creature.js
@@ -170,13 +170,26 @@
     }
 
     /* [Function] Search */
-    $scope.search = function (CreatureEntry, CreatureName, CreatureSubname) {
+    $scope.search = function (creatureEntry, creatureName, creatureSubname) {
+
+      if ( creatureEntry && (!creatureName && !creatureSubname) && (creatureEntry.length < 2) ) {
+        alert("Please insert an Entry of at least 3 characters");
+        return;
+      }
+      if ( creatureName && (!creatureEntry && !creatureSubname) && (creatureName.length < 3) ) {
+        alert("Please insert a Name of at least 3 characters");
+        return;
+      }
+      if ( creatureSubname && (!creatureEntry && !creatureName) && (creatureSubname.length < 3) ) {
+        alert("Please insert a Subname of at least 3 characters");
+        return;
+      }
 
       $http.get( app.api + "/search/creature/", {
         params: {
-          id: CreatureEntry,
-          name: CreatureName,
-          subname: CreatureSubname
+          id: creatureEntry,
+          name: creatureName,
+          subname: creatureSubname
         }
       }).success(function (data, status, header, config) {
         $scope.creatures = data;
