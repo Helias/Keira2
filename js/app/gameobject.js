9822d75234033d50de5146f858be7dd52d2369cc
Helias <stefanoborzi32@gmail.com>
2015-05-20 23:36:58 +0200

    Added gameobject structure (to complete)

--- js/app/gameobject.js
+++ js/app/gameobject.js
@@ -1,4 +1,4 @@
-/*jslint browser: true, white: true, plusplus: true*/
+/*jslint browser: true, white: true, plusplus: true */
 /*global angular, console, alert*/
 
 (function () {
@@ -6,36 +6,110 @@
 
   var app = angular.module('keira2');
 
-  app.controller("GameobjectController", function ($scope, $http) {
-    /*
-    $scope.search = function (GameobjectEntry, GameobjectName) {
-      var request = app.api + "gameobject/template/";
+  app.controller("GameobjectController", function ($scope, $http, $stateParams, $modal) {
 
-      if ((typeof GameobjectEntry !== "undefined") && (GameobjectEntry !== "")) {
+    /* All Gameobject tabs, disabled by default.
+    *  Only one tab can be active at a time */
+    $scope.gameobjectTabs = {
+      search              : false,
+      gameobjectTemplate  : false,
+      spawns              : false,
+      loot                : false,
+      script              : false
+    };
 
-        request += GameobjectEntry;
+    /* Init arrays */
+    $scope.current_gameobject_template      = [];
+    $scope.new_gameobject_template          = [];
+    $scope.current_spawns                   = [];
+    $scope.new_spawns                       = [];
+    $scope.current_loot                     = [];
+    $scope.new_loot                         = [];
 
-      } else if (typeof GameobjectName !== "undefined") {
+    /* Check if a gameobject is selected */
+    if ($stateParams.id) {
 
-        request += GameobjectName;
+      /* We have a gameobject selected and default active tab is gameobjectTemplate */
+      $scope.isGameobjectSelected = true;
+      $scope.gameobjectTabs.gameobjectTemplate = true;
 
-      } else {
+      /*  Following lines retrieve all Gameobject datas
+       *  current_* mantains the database state
+       *  new_*     mantains the editor state
+       *  we will use those two objects to generate the SQL queries
+       */
 
-        alert("Insert Entry or Name of a gameobject");
+      /* Retrieve all gameobject_template datas */
+      $http.get( app.api + "gameobject/template/" + $stateParams.id)
+        .success(function (data, status, header, config) {
+        $scope.current_gameobject_template = data[0];
+        $scope.new_gameobject_template = angular.copy($scope.current_gameobject_template);
+      })
+        .error(function (data, status, header, config) {
+        console.log("[ERROR] gameobject/template/" + $stateParams.id + " $http.get request failed");
+      });
+
+    } else {
+      /* We have no gameobject selected and default active tab is search */
+      $scope.isGameobjectSelected = false;
+      $scope.gameobjectTabs.search = true;
+    }
+
+    /* [Function] Search */
+    $scope.search = function (gameobjectEntry, gameobjectName) {
+
+      if ( gameobjectEntry && !gameobjectName && (gameobjectEntry.length < 2) ) {
+        alert("Please insert an ID of at least 2 characters");
         return;
-
+      }
+      if ( gameobjectName && !gameobjectEntry && (gameobjectName.length < 3) ) {
+        alert("Please insert a Title of at least 3 characters");
+        return;
       }
 
-      $http.get(request)
-        .success(function (data, status, header, config) {
+      $http.get( app.api + "/search/gameobject/", {
+        params: {
+          id: gameobjectEntry,
+          name: gameobjectName
+        }
+      }).success(function (data, status, header, config) {
         $scope.gameobjects = data;
       })
         .error(function (data, status, header, config) {
-        console.log("Error in GAMEOBJECT SEARCH $http.get request");
+        console.log("[ERROR] GAMEOBJECT SEARCH $http.get request failed");
       });
 
     };
-    */
+
+    /* [Function] Generate SQL Script for Gameobject */
+    $scope.generateGameobjectScript = function() {
+
+      if (!$scope.isGameobjectSelected) {
+        $scope.gameobjectScript = "# No Gameobject selected";
+        return;
+      }
+
+      $scope.gameobjectScript = "";
+
+      var whereCondition = "Entry = " + $scope.current_gameobject_template.Entry;
+
+      $scope.gameobjectScript += app.getUpdateQuery("gameobject_template", whereCondition, $scope.current_gameobject_template, $scope.new_gameobject_template);
+    };
+
+    /* [Function] disactive all tabs */
+    $scope.disactiveAllTabs = function() {
+      angular.forEach($scope.gameobjectTabs, function(value, key) {
+        value = false;
+      });
+    };
+
+    /* [Function] open SQL Script tab */
+    $scope.openScriptTab = function() {
+      $scope.disactiveAllTabs();
+      $scope.gameobjectScript();
+      $scope.gameobjectTabs.script = true;
+    };
+
   });
 
 }());
