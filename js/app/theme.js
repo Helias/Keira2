ffe58c0cedd13f220815e7f176de2221e53c06dc
ShinDarth <borzifrancesco@gmail.com>
2015-05-21 02:48:17 +0200

    Add more themes

--- js/app/theme.js
+++ js/app/theme.js
@@ -15,12 +15,17 @@
       { name: "Cerulean", url: "css/themes/bootswatch/3.3.4/cerulean.css"},
       { name: "Cosmo", url: "css/themes/bootswatch/3.3.4/cosmo.css"},
       { name: "Cyborg", url: "css/themes/bootswatch/3.3.4/cyborg.css"},
+      { name: "Darkly", url: "css/themes/bootswatch/3.3.4/darkly.css"},
       { name: "Flatly", url: "css/themes/bootswatch/3.3.4/flatly.css"},
       { name: "Journal", url: "css/themes/bootswatch/3.3.4/journal.css"},
+      { name: "Lumen", url: "css/themes/bootswatch/3.3.4/lumen.css"},
+      { name: "Paper", url: "css/themes/bootswatch/3.3.4/paper.css"},
       { name: "Readable", url: "css/themes/bootswatch/3.3.4/readable.css"},
+      { name: "Sandstone", url: "css/themes/bootswatch/3.3.4/sandstone.css"},
       { name: "Simplex", url: "css/themes/bootswatch/3.3.4/simplex.css"},
       { name: "Slate", url: "css/themes/bootswatch/3.3.4/slate.css"},
       { name: "Spacelab", url: "css/themes/bootswatch/3.3.4/spacelab.css"},
+      { name: "Superhero", url: "css/themes/bootswatch/3.3.4/superhero.css"},
       { name: "United", url: "css/themes/bootswatch/3.3.4/united.css"},
       { name: "Yeti", url: "css/themes/bootswatch/3.3.4/yeti.css"}
     ];
