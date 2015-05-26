544b3131e043da21c7263306fdce4da34c8cca27
ShinDarth <borzifrancesco@gmail.com>
2015-05-26 23:45:27 +0200

    Added SAI basic files

--- js/app/sai.js
+++ js/app/sai.js
@@ -8,6 +8,98 @@
 
   app.controller("SmartAIController", function ($scope, $http, $stateParams, $modal) {
 
+    $scope.sourceType = $stateParams.sourceType;
+    $scope.entryOrGuid = $stateParams.sourceType;
+
+
+    /* All SAI tabs, disabled by default.
+    *  Only one tab can be active at a time */
+    $scope.saiTabs = {
+      search : false,
+      editor : false,
+      script : false
+    };
+
+    /* Init arrays */
+    $scope.smart_scripts       = [];
+
+    /* Check if an entity (smart_scripts.source_type AND smart_scripts.entryorguid) is selected */
+    if ($stateParams.sourceType && $stateParams.entryOrGuid) {
+
+      /* We have a creature selected and default active tab is creatureTemplate */
+      $scope.isEntitySelected = true;
+      $scope.saiTabs.editor = true;
+
+      /*  Following lines retrieve all SAI datas
+       *  current_* mantains the database state
+       *  new_*     mantains the editor state
+       *  we will use those two objects to generate the SQL queries
+       */
+
+      /* Retrieve all smart_scripts datas */
+      $http.get( app.api + "smart_scripts/" + $stateParams.sourceType + "/" + $stateParams.entryOrGuid )
+        .success(function (data, status, header, config) {
+        $scope.current_smart_scripts = data[0];
+        $scope.new_smart_scripts = angular.copy($scope.current_smart_scripts);
+
+        // TODO improve this:
+        $scope.selectionText = $scope.current_smart_scripts.sourceType + "/" + $scope.current_smart_scripts.entryOrGuid;
+      })
+        .error(function (data, status, header, config) {
+        console.log("[ERROR] smart_scripts/" + $stateParams.sourceType + "/" + $stateParams.entryOrGuid + " $http.get request failed");
+      });
+
+    } else {
+      /* We have no creature selected and default active tab is search */
+      $scope.isEntitySelected = false;
+      $scope.saiTabs.search = true;
+      $scope.selectionText = "No entity selected. Please use Search to select one.";
+    }
+
+    /* [Function] Search */
+    $scope.search = function (sourceType, entryOrGuid) {
+
+      // TODO: search among not-yet-scripted entities
+
+      $http.get( app.api + "/search/smart_scripts/", {
+        params: {
+          source_type: sourceType,
+          entryorguid: entryOrGuid
+        }
+      }).success(function (data, status, header, config) {
+        $scope.scripts = data;
+      })
+        .error(function (data, status, header, config) {
+        console.log("[ERROR] SMART SCRIPTS SEARCH $http.get request failed");
+      });
+
+    };
+
+    /* [Function] Generate SQL SAI Script */
+    $scope.generateSAIScript = function() {
+
+      if (!$scope.isEntitySelected) {
+        $scope.SAIScript = "# No entity selected";
+        return;
+      }
+
+      // TODO: generate sql full script
+    };
+
+    /* [Function] disactive all tabs */
+    $scope.disactiveAllTabs = function() {
+      angular.forEach($scope.saiTabs, function(value, key) {
+        value = false;
+      });
+    };
+
+    /* [Function] open SQL Script tab */
+    $scope.openScriptTab = function() {
+      $scope.disactiveAllTabs();
+      $scope.generateSAIScript();
+      $scope.saiTabs.script = true;
+    };
+
   });
 
 }());
