f65c84aa5b59d32988e0e195ace853ea3a62ab29
ShinDarth <borzifrancesco@gmail.com>
2015-05-29 23:20:18 +0200

    Init SAI Editor controller

---
+++ js/app/sai/editor.js
@@ -0,0 +1,125 @@
+/*jslint browser: true, eqeq: true, white: true, plusplus: true */
+/*global angular, console, alert*/
+
+(function () {
+  'use strict';
+
+  var app = angular.module('keira2');
+
+  app.controller("SmartAIEditorController", function ($scope, $rootScope, $stateParams) {
+
+    /* At start we have no row selected */
+    $scope.selectedRow = -1;
+
+    /* The item currently selected by the user (bound to the view) */
+    $scope.selected = {
+      entryorguid       : parseInt($stateParams.entryOrGuid, 10),
+      source_type       : parseInt($stateParams.sourceType, 10),
+      id                : 0,
+      link              : 0,
+      event_type        : 0,
+      event_phase_mask  : 0,
+      event_chance      : 100,
+      event_flags       : 0,
+      event_param1      : 0,
+      event_param2      : 0,
+      event_param3      : 0,
+      event_param4      : 0,
+      action_type       : 0,
+      action_param1     : 0,
+      action_param2     : 0,
+      action_param3     : 0,
+      action_param4     : 0,
+      action_param5     : 0,
+      action_param6     : 0,
+      target_type       : 0,
+      target_param1     : 0,
+      target_param2     : 0,
+      target_param3     : 0,
+      target_x          : 0.0,
+      target_y          : 0.0,
+      target_z          : 0.0,
+      target_o          : 0.0,
+      comment           : 'Event Comment'
+    };
+
+    /* Type check */
+    $scope.parseValues = function() {
+
+      $scope.selected.id                = parseInt($scope.selected.id, 10);
+      $scope.selected.link              = parseInt($scope.selected.link, 10);
+      $scope.selected.event_type        = parseInt($scope.selected.event_type, 10);
+      $scope.selected.event_phase_mask  = parseInt($scope.selected.event_phase_mask, 10);
+      $scope.selected.event_chance      = parseInt($scope.selected.event_chance, 10);
+      $scope.selected.event_flags       = parseInt($scope.selected.event_flags, 10);
+      $scope.selected.event_param1      = parseInt($scope.selected.event_param1, 10);
+      $scope.selected.event_param2      = parseInt($scope.selected.event_param2, 10);
+      $scope.selected.event_param3      = parseInt($scope.selected.event_param3, 10);
+      $scope.selected.event_param4      = parseInt($scope.selected.event_param4, 10);
+      $scope.selected.action_type       = parseInt($scope.selected.action_type, 10);
+      $scope.selected.action_param1     = parseInt($scope.selected.action_param1, 10);
+      $scope.selected.action_param2     = parseInt($scope.selected.action_param2, 10);
+      $scope.selected.action_param3     = parseInt($scope.selected.action_param3, 10);
+      $scope.selected.action_param4     = parseInt($scope.selected.action_param4, 10);
+      $scope.selected.action_param5     = parseInt($scope.selected.action_param5, 10);
+      $scope.selected.action_param6     = parseInt($scope.selected.action_param6, 10);
+      $scope.selected.target_type       = parseInt($scope.selected.target_type, 10);
+      $scope.selected.target_param1     = parseInt($scope.selected.target_param1, 10);
+      $scope.selected.target_param2     = parseInt($scope.selected.target_param2, 10);
+      $scope.selected.target_param3     = parseInt($scope.selected.target_param3, 10);
+      $scope.selected.target_x          = parseFloat($scope.selected.target_x, 10);
+      $scope.selected.target_y          = parseFloat($scope.selected.target_y, 10);
+      $scope.selected.target_z          = parseFloat($scope.selected.target_z, 10);
+      $scope.selected.target_o          = parseFloat($scope.selected.target_o, 10);
+    };
+
+    /* Select a row from collection */
+    $scope.selectRow = function(rows, index) {
+      $scope.selectedRow = index;
+      $scope.selected = angular.copy(rows[index]);
+    };
+
+    /* Edit selected row */
+    $scope.editSelectedRowOf = function(rows, primaryKey2) {
+      if (!$scope.isEntrySelected()) { return; }
+      var i;
+      $scope.parseValues();
+
+      // check primaryKey2 uniqueness
+      for (i = 0; i < rows.length; i++) {
+        if ( (rows[i][primaryKey2] == $scope.selected[primaryKey2]) && (i !== $scope.selectedRow) ) {
+          alert("Duplicate row with `" + primaryKey2 + "` = " + $scope.selected[primaryKey2]);
+          return;
+        }
+      }
+
+      rows.splice($scope.selectedRow, 1, angular.copy($scope.selected));
+    };
+
+    /* Delete selected row from collection */
+    $scope.deleteSelectedRowFrom = function(rows) {
+      if (!$rootScope.isEntrySelected()) { return; }
+
+      rows.splice($scope.selectedRow, 1);
+    };
+
+    /* Add selected row to collection */
+    $scope.addRowTo = function(rows, primaryKey2) {
+      if (!$rootScope.isEntrySelected()) { return; }
+      var i;
+      $scope.parseValues();
+
+      // check primaryKey2 uniqueness
+      for (i = 0; i < rows.length; i++) {
+        if (rows[i][primaryKey2] == $scope.selected[primaryKey2]) {
+          alert("Duplicate row with `" + primaryKey2 + "` = " + $scope.selected[primaryKey2]);
+          return;
+        }
+      }
+
+      rows.splice(0, 0, angular.copy($scope.selected));
+    };
+
+  });
+
+}());
