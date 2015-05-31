5709f2da324be9367444b668490764ecf3034d9e
ShinDarth <borzifrancesco@gmail.com>
2015-05-31 15:48:42 +0200

    SAI: implemented creation of a Creature or Gameobject new script

--- js/app/sai/create-new.js
+++ js/app/sai/create-new.js
@@ -6,10 +6,41 @@
 
   var app = angular.module('keira2');
 
-  app.controller("CreateNewSAIController", function ($scope, $rootScope, $stateParams) {
+  app.controller("CreateNewSAIController", function ($scope, $rootScope, $stateParams, $http) {
 
     /* Default selectedSourceType */
     $scope.selectedSourceType = 0;
+
+    /* [Function] searchCreatures*/
+    $scope.searchCreatures = function (creatureEntry, creatureName, creatureSubname) {
+
+      if ( creatureEntry && (!creatureName && !creatureSubname) && (creatureEntry.length < 2) ) {
+        alert("Please insert an Entry of at least 2 characters");
+        return;
+      }
+      if ( creatureName && (!creatureEntry && !creatureSubname) && (creatureName.length < 3) ) {
+        alert("Please insert a Name of at least 3 characters");
+        return;
+      }
+      if ( creatureSubname && (!creatureEntry && !creatureName) && (creatureSubname.length < 3) ) {
+        alert("Please insert a Subname of at least 3 characters");
+        return;
+      }
+
+      $http.get( app.api + "/search/creature/", {
+        params: {
+          id: creatureEntry,
+          name: creatureName,
+          subname: creatureSubname
+        }
+      }).success(function (data, status, header, config) {
+        $scope.creatures = $rootScope.fixNumericValues(data);
+      })
+        .error(function (data, status, header, config) {
+        console.log("[ERROR] CREATURE SEARCH $http.get request failed");
+      });
+
+    };
 
   });
 
