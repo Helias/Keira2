e9a49d1831c152905f2ec4c11056786081964212
ShinDarth <borzifrancesco@gmail.com>
2015-05-31 15:23:44 +0200

    SAI: init Create new structure

--- js/app/sai/create-new.js
+++ js/app/sai/create-new.js
@@ -7,6 +7,10 @@
   var app = angular.module('keira2');
 
   app.controller("CreateNewSAIController", function ($scope, $rootScope, $stateParams) {
+
+    /* Default selectedSourceType */
+    $scope.selectedSourceType = 0;
+
   });
 
 }());
