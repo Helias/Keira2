89726a2eb2a2a88d31e78afc354ba8e2d19ac95b
ShinDarth <borzifrancesco@gmail.com>
2015-06-29 13:03:47 +0200

    Fit with latest TrinityCore JSON API

--- js/app/functions.js
+++ js/app/functions.js
@@ -194,7 +194,7 @@
   app.synchGetSpellNameById = function(id) {
     var request, data;
     request = new XMLHttpRequest();
-    request.open('GET', app.api + "/dbc/spells_wotlk/" + id, false);
+    request.open('GET', app.api + "/dbc/spells/" + id, false);
     request.send(null);
 
     if (request.status === 200) {
