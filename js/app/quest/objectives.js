21c30a4df21cee1a7adf22621ac16e619697e66d
ShinDarth <borzifrancesco@gmail.com>
2015-09-11 18:03:19 +0200

    Quest: implemented Quest Objectives tab

---
+++ js/app/quest/objectives.js
@@ -0,0 +1,90 @@
+/*jslint browser: true, eqeq: true, white: true, plusplus: true */
+/*global angular, console, alert*/
+
+(function () {
+  'use strict';
+
+  var app = angular.module('keira2');
+
+  app.controller("QuestObjectivesController", function ($scope, $rootScope, $stateParams) {
+
+    /* At start we have no row selected */
+    $scope.selectedRow = -1;
+
+    /* The item currently selected by the user (bound to the view) */
+    $scope.selected = {
+      QuestID       : parseInt($stateParams.id, 10),
+      ID            : 0,
+      Type          : 0,
+      StorageIndex  : 0,
+      ObjectID      : 0,
+      Amount        : 0,
+      Flags         : 0,
+      Description   : '',
+      UnkFloat      : 0,
+      VerifiedBuild : 0
+    };
+
+    /* Type check */
+    $scope.parseValues = function() {
+
+      $scope.selected.ID            = parseInt($scope.selected.ID, 10);
+      $scope.selected.Type          = parseInt($scope.selected.Type, 10);
+      $scope.selected.StorageIndex  = parseInt($scope.selected.StorageIndex, 10);
+      $scope.selected.ObjectID      = parseInt($scope.selected.ObjectID, 10);
+      $scope.selected.Amount        = parseInt($scope.selected.Amount, 10);
+      $scope.selected.Flags         = parseInt($scope.selected.Flags, 10);
+      $scope.selected.UnkFloat      = parseInt($scope.selected.UnkFloat, 10);
+      $scope.selected.VerifiedBuild = parseInt($scope.selected.VerifiedBuild, 10);
+    };
+
+    /* Select a row from collection */
+    $scope.selectRow = function(rows, index) {
+      $scope.selectedRow = index;
+      $scope.selected = angular.copy(rows[index]);
+    };
+
+    /* Edit selected row */
+    $scope.editSelectedRowOf = function(rows, primaryKey2) {
+      if (!$scope.isEntrySelected()) { return; }
+      var i;
+      $scope.parseValues();
+
+      // check primaryKey2 uniqueness
+      for (i = 0; i < rows.length; i++) {
+        if ( (rows[i][primaryKey2] == $scope.selected[primaryKey2]) && (i !== $scope.selectedRow) ) {
+          alert("Duplicate row with `" + primaryKey2 + "` = " + $scope.selected[primaryKey2]);
+          return;
+        }
+      }
+
+      rows.splice($scope.selectedRow, 1, angular.copy($scope.selected));
+    };
+
+    /* Delete selected row from collection */
+    $scope.deleteSelectedRowFrom = function(rows) {
+      if (!$rootScope.isEntrySelected()) { return; }
+
+      rows.splice($scope.selectedRow, 1);
+    };
+
+    /* Add selected row to collection */
+    $scope.addRowTo = function(rows, primaryKey2) {
+      if (!$rootScope.isEntrySelected()) { return; }
+      var i;
+      $scope.parseValues();
+
+      // check primaryKey2 uniqueness
+      for (i = 0; i < rows.length; i++) {
+        if (rows[i][primaryKey2] == $scope.selected[primaryKey2]) {
+          alert("Duplicate row with `" + primaryKey2 + "` = " + $scope.selected[primaryKey2]);
+          return;
+        }
+      }
+
+      rows.splice(0, 0, angular.copy($scope.selected));
+    };
+
+  });
+
+}());
